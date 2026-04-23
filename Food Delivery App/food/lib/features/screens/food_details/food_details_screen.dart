6e689cba49a5549e2ba82410ec5cfa930871f262
mdrahib46 <nahid179846@bsdi-bd.org>
2026-04-24 00:57:35 +0600

    feat :Food Details Screen

--- Food Delivery App/food/lib/features/screens/food_details/food_details_screen.dart
+++ Food Delivery App/food/lib/features/screens/food_details/food_details_screen.dart
@@ -1,17 +1,17 @@
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
     
     
-    class FoodDetailsScreen extends StatefulWidget {
-      const FoodDetailsScreen({super.key});
-    
-      @override
-      State<FoodDetailsScreen> createState() => _FoodDetailsScreenState();
-    }
-    
-    class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(appBar: AppBar(title: Text('Food Details Scren'),),);
-      }
-    }
+class FoodDetailsScreen extends StatefulWidget {
+  const FoodDetailsScreen({super.key});
+
+  @override
+  State<FoodDetailsScreen> createState() => _FoodDetailsScreenState();
+}
+
+class _FoodDetailsScreenState extends State<FoodDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('Food Details Scren'),),);
+  }
+}
     
