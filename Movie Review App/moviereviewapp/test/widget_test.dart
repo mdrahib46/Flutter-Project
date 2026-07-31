6f64ef3a7575dc6fac568305849a285b8f68f89c
mdrahib46 <nahid179846@bsdi-bd.org>
2026-07-31 23:29:56 +0600

    Intro : Movie Review App Project

--- Movie Review App/moviereviewapp/test/widget_test.dart
+++ Movie Review App/moviereviewapp/test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:moviereviewapp/app/movie_review_app.dart';
 
-import 'package:moviereviewapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
