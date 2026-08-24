dbf98c07a3f14212257b7a3c3234627344dfa8d6
mdrahib46 <nahid179846@bsdi-bd.org>
2026-08-24 14:28:41 +0600

    Search Screen Has Completed

--- Movie Review App/moviereviewapp/lib/features/search/presentation/screen/search_screen.dart
+++ Movie Review App/moviereviewapp/lib/features/search/presentation/screen/search_screen.dart
@@ -20,89 +20,94 @@ class _SearchScreenState extends State<SearchScreen> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: 'Search....',
-                  suffixIcon: Icon(Icons.search),
-                ),
-              ),
-              const SizedBox(height: 16),
-              MovieSectionHeader(title: 'Trending this month', onTap: () {}),
-              const SizedBox(height: 8),
-              SizedBox(
-                height: 190,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) => const Padding(
-                    padding: EdgeInsets.only(right: 12),
-                    child: SizedBox(width: 120, child: MovieCard()),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: 'Search....',
+                    suffixIcon: Icon(Icons.search),
                   ),
                 ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                height: 100,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  gradient: LinearGradient(
-                    begin: AlignmentGeometry.topLeft,
-                    end: AlignmentGeometry.bottomRight,
-                    colors: [
-                      Colors.white.withAlpha(99),
-                      Colors.white.withAlpha(20),
-                    ],
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withAlpha(40),
-                      blurRadius: 10,
-                      spreadRadius: 2,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    spacing: 8,
-                    children: [
-                      HugeIcon(
-                        icon: HugeIcons.strokeRoundedPlayCircle02,
-                        color: Colors.white,
-                        size: 60,
-                      ),
-                      Text('Brows All Movies', style: TextStyle(fontSize: 26)),
-                      const Spacer(),
-                      Icon(Icons.arrow_forward, size: 40, color: Colors.white),
-                    ],
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 16),
-
-              MovieSectionHeader(title: 'Upcomming Movies', onTap: () {}),
-
-              SizedBox(
-                height: 220,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return const Padding(
+                const SizedBox(height: 16),
+                MovieSectionHeader(title: 'Trending this month', onTap: () {}),
+                const SizedBox(height: 8),
+                SizedBox(
+                  height: 190,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (context, index) => const Padding(
                       padding: EdgeInsets.only(right: 12),
-                      child: UpcomingMovieCard(),
-                    );
-                  },
+                      child: SizedBox(width: 120, child: MovieCard()),
+                    ),
+                  ),
                 ),
-              ),
-
-
-            ],
+                const SizedBox(height: 16),
+                Container(
+                  height: 100,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    gradient: LinearGradient(
+                      begin: AlignmentGeometry.topLeft,
+                      end: AlignmentGeometry.bottomRight,
+                      colors: [
+                        Colors.white.withAlpha(99),
+                        Colors.white.withAlpha(20),
+                      ],
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(40),
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: const Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Row(
+                      spacing: 8,
+                      children: [
+                        HugeIcon(
+                          icon: HugeIcons.strokeRoundedPlayCircle02,
+                          color: Colors.white,
+                          size: 60,
+                        ),
+                        Text('Brows All Movies', style: TextStyle(fontSize: 26)),
+                        const Spacer(),
+                        Icon(Icons.arrow_forward, size: 40, color: Colors.white),
+                      ],
+                    ),
+                  ),
+                ),
+            
+                const SizedBox(height: 16),
+            
+                MovieSectionHeader(title: 'Upcoming Movies', onTap: () {}),
+                Text('Add to the wishlist'),
+                const SizedBox(height: 16,),
+            
+                SizedBox(
+                  height: 220,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return const Padding(
+                        padding: EdgeInsets.only(right: 12),
+                        child: UpcomingMovieCard(),
+                      );
+                    },
+                  ),
+                ),
+            
+            
+              ],
+            ),
           ),
         ),
       ),
